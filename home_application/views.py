c8648d8f2cabc705480e8eec3ffffe44780edd02
mint <619866035@qq.com>
2026-05-18 22:45:29 +0800

    feat: 后端

--- home_application/views.py
+++ home_application/views.py
@@ -1,94 +1,189 @@
 # -*- coding: utf-8 -*-
-"""
-Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
-Edition) available.
-Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://opensource.org/licenses/MIT
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-"""
+import os
+
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from blueking.component.shortcuts import get_client_by_request
+from blueapps.account.decorators import login_exempt
+from blueking.component.shortcuts import get_client_by_request, get_client_by_user
+from home_application.models import BizInfo
 
 
-# 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
-# 装饰器引入 from blueapps.account.decorators import login_exempt
+api_login_exempt = login_exempt if settings.DEBUG else lambda func: func
+
+
 def home(request):
-    """
-    首页
-    """
     return render(request, "home_application/index_home.html")
 
 
 def dev_guide(request):
-    """
-    开发指引
-    """
     return render(request, "home_application/dev_guide.html")
 
 
 def contact(request):
-    """
-    联系页
-    """
     return render(request, "home_application/contact.html")
 
 
-# 开发样例，在这里实现了【查询业务列表接口】
+def _param_as_int(request, name):
+    value = request.GET.get(name)
+    if value in (None, ""):
+        return None
+    try:
+        return int(value)
+    except (TypeError, ValueError):
+        return None
+
+
+def _missing_param_response(name):
+    return JsonResponse({
+        "result": False,
+        "message": "missing required parameter: {}".format(name),
+        "data": {"count": 0, "info": []},
+    }, status=400)
+
+
+def _get_component_client(request):
+    bk_token = request.COOKIES.get("bk_token")
+    if bk_token:
+        return get_client_by_request(request, bk_token=bk_token)
+    if settings.DEBUG:
+        return get_client_by_user(os.getenv("BK_DEV_USERNAME", "admin"))
+    return get_client_by_request(request)
+
+
+@api_login_exempt
 def get_bizs_list(request):
-    """
-    获取业务列表
-    """
-    # 从环境配置获取APP信息，从request获取当前用户信息
-    client = get_client_by_request(request)
-    # 请求参数
-    kwargs = {
-        "fields": [
-            "bk_biz_id",
-            "bk_biz_name"
-        ],
-        # 社区版环境中业务数量有限，故不考虑分页情况
+    """Fetch the business list from local cache first, then CMDB."""
+    bizs = BizInfo.objects.all().order_by("bk_biz_id")
+    if bizs.exists():
+        return JsonResponse({
+            "result": True,
+            "message": "success",
+            "data": {
+                "count": bizs.count(),
+                "info": list(bizs.values("bk_biz_id", "bk_biz_name")),
+            },
+        })
+
+    client = _get_component_client(request)
+    result = client.cc.search_business({
+        "fields": ["bk_biz_id", "bk_biz_name"],
         "page": {
             "start": 0,
-            "limit": 10,
-            "sort": ""
-        }
-    }
-    # 这里需要填写对应的组件API的入口地址
-    result = client.cc.search_business(kwargs)
+            "limit": 100,
+            "sort": "",
+        },
+    })
+
+    if result.get("result") and result.get("data"):
+        for biz in result["data"].get("info", []):
+            BizInfo.objects.update_or_create(
+                bk_biz_id=biz["bk_biz_id"],
+                defaults={"bk_biz_name": biz["bk_biz_name"]},
+            )
     return JsonResponse(result)
 
 
-# 请仿照此前的接口，实现查询集群列表接口
+@api_login_exempt
 def get_sets_list(request):
-    """
-    根据业务ID，查询业务下的集群列表
-    """
-    pass  # 请实现你的接口
+    """Fetch set list by business id."""
+    bk_biz_id = _param_as_int(request, "bk_biz_id")
+    if bk_biz_id is None:
+        return _missing_param_response("bk_biz_id")
+
+    client = _get_component_client(request)
+    result = client.cc.search_set({
+        "bk_biz_id": bk_biz_id,
+        "fields": [
+            "bk_set_id",
+            "bk_set_name",
+            "bk_biz_id",
+            "bk_created_at",
+            "bk_supplier_account",
+        ],
+    })
+    return JsonResponse(result)
 
 
+@api_login_exempt
 def get_modules_list(request):
-    """
-    根据业务ID和集群ID，查询对应下辖的模块列表
-    """
-    pass  # 请实现你的接口
+    """Fetch module list by business id and set id."""
+    bk_biz_id = _param_as_int(request, "bk_biz_id")
+    bk_set_id = _param_as_int(request, "bk_set_id")
+    if bk_biz_id is None:
+        return _missing_param_response("bk_biz_id")
+    if bk_set_id is None:
+        return _missing_param_response("bk_set_id")
+
+    client = _get_component_client(request)
+    result = client.cc.search_module({
+        "bk_biz_id": bk_biz_id,
+        "bk_set_id": bk_set_id,
+        "fields": [
+            "bk_module_id",
+            "bk_module_name",
+            "bk_set_id",
+            "bk_biz_id",
+            "bk_created_at",
+            "bk_supplier_account",
+        ],
+    })
+    return JsonResponse(result)
 
 
+@api_login_exempt
 def get_hosts_list(request):
-    """
-    根据传递的查询条件，包括但不限于（业务ID、集群ID、模块ID、主机ID、主机维护人）
-    查询主机列表
-    """
-    pass  # 请实现你的接口
+    """Fetch host list by business id and optional set/module/operator filters."""
+    bk_biz_id = _param_as_int(request, "bk_biz_id")
+    if bk_biz_id is None:
+        return _missing_param_response("bk_biz_id")
+
+    kwargs = {
+        "bk_biz_id": bk_biz_id,
+        "page": {
+            "start": 0,
+            "limit": 100,
+        },
+        "fields": [
+            "bk_host_id",
+            "bk_host_innerip",
+            "operator",
+            "bk_bak_operator",
+        ],
+    }
+
+    bk_set_id = _param_as_int(request, "bk_set_id")
+    if bk_set_id is not None:
+        kwargs["bk_set_ids"] = [bk_set_id]
+
+    bk_module_id = _param_as_int(request, "bk_module_id")
+    if bk_module_id is not None:
+        kwargs["bk_module_ids"] = [bk_module_id]
+
+    operator = request.GET.get("operator")
+    if operator:
+        kwargs["host_property_filter"] = {
+            "condition": "AND",
+            "rules": [{
+                "field": "operator",
+                "operator": "contains",
+                "value": operator,
+            }],
+        }
+
+    client = _get_component_client(request)
+    result = client.cc.list_biz_hosts(kwargs)
+    return JsonResponse(result)
 
 
+@api_login_exempt
 def get_host_detail(request):
-    """
-    根据主机ID，查询主机详情信息
-    """
-    pass  # 请实现你的接口
+    """Fetch host detail by host id."""
+    bk_host_id = _param_as_int(request, "bk_host_id")
+    if bk_host_id is None:
+        return _missing_param_response("bk_host_id")
+
+    client = _get_component_client(request)
+    result = client.cc.get_host_base_info({"bk_host_id": bk_host_id})
+    return JsonResponse(result)
